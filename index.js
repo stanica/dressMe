1e447c46bb41b93db17ea4f7db8cbc6afe9ccac8
Pierre Beaucamp <mail@pierrebeaucamp.com>
2016-03-12 22:18:49 -0500

    Basic selection algorithm

--- index.js
+++ index.js
@@ -220,28 +220,53 @@ function getSelfInfo(intent, session, callback) {
 /*
  * Main algorithm, markov chain and stuff...
  */
-function findCombination(result, intent, session, callback) {
-    var sessionAttributes = getSessionAttributes(session);
-    var repromptText      = null;
-    var shouldEndSession  = false;
-    var speechOutput      = "Hello, World";
+function findCombination(result) {
+    var out = "your " + result.color + " " + result.description + " " +
+               result.type;
 
-    callback(
-        sessionAttributes,
-        buildSpeechletResponse(
-            intent.name, speechOutput, repromptText, shouldEndSession
-        )
-    );
+    if (result.fullbody) return out + ".";
+
+    var q    = "SELECT * FROM bottoms;";
+    conn.query(q, function(err, rows, fields) {
+        if (err) throw err;
+
+        if (result.combination && result.combination != "{}") {
+            json = JSON.parse(result.combination);
+            for (var i = 0; i < rows.length; i++) {
+                if (json[rows[i].id]) {
+                    for (var j = 0; j < json[rows[i].id]; j++) {
+                        rows.push(rows[i].id);
+                    }
+                }
+            }
+        }
+
+        var bottom = rows[Math.floor(Math.random() * rows.length)];
+        return out + " together with your " + bottom.color + " " +
+               bottom.description + " " + bottom.description + ".";
+    });
 }
 
 /*
  * Main DressMe handler
  */
 function handleDressMe(situation, description, intent, session, callback) {
-    var q = "SELECT * FROM tops;"
-    var resp = conn.query(q, function(err, rows, fields) {
+    var sessionAttributes = getSessionAttributes(session);
+    var repromptText      = null;
+    var shouldEndSession  = false;
+    var speechOutput      = "What about ";
+
+    var q = "SELECT * FROM tops ORDER BY RAND() LIMIT 1;"
+    conn.query(q, function(err, rows, fields) {
         if (err) throw err;
-        findCombination(rows[0], intent, session, callback);
+        speechOutput += findCombination(rows[0]);
+
+        callback(
+            sessionAttributes,
+            buildSpeechletResponse(
+                intent.name, speechOutput, repromptText, shouldEndSession
+            )
+        );
     });
 }
 
@@ -273,7 +298,9 @@ function handleAddClothes(intent, session, callback) {
     var shouldEndSession = false;
     var parsedClothesInfo = parseClothes(intent.slots.Clothes.value);
 
-    var speechOutput = "I have added your clothes. Color is " + parsedClothesInfo.color + ". Article type is " + parsedClothesInfo.article + ". Description is " + parsedClothesInfo.description.join(" ") + ".";
+    var speechOutput = "I have added your clothes. Color is " +
+        parsedClothesInfo.color + ". Article type is " + parsedClothesInfo.article +
+        ". Description is " + parsedClothesInfo.description.join(" ") + ".";
     sessionAttributes.clothes.push(intent.slots.Clothes.value);
     // Setting repromptText to null signifies that we do not want to reprompt the user.
     // If the user does not respond or says something that is not understood, the session
@@ -349,105 +376,103 @@ function buildResponse(sessionAttributes, speechletResponse) {
 var colorQualifiers = ["light", "dark", "pale"];
 
 var clothes = {
-	//top
-	'tank top': ["top",  20, 45],
-	't shirt': ["top", 20, 45],
-	'tee': ["top", 20, 45],
-  'shirt':["top", 20, 45],
-	'long sleeves shirt': ["top", 10, 20],
-	'long sleeve shirt': ["top", 10, 20],
-  'long sleeved shirt': ["top", 10, 20],
-  'long sleeve dress shirt': ["top", 10, 20],
-  'long sleeves dress shirt': ["top", 10, 20],
-  'long sleeved dress shirt': ["top", 10, 20],
-	'blouse': ["top", 10, 20],
-	'sweater': ["top", 0, 10],
-	'sweatshirt': ["top", 10, 20],
-	'dress shirt': ["top", "formal", -30, 20],
-	'dress T shirt': ["top", 20, 45],
-	'polo': ["top", 20, 45],
-	'crop top': ["top", 20, 45],
-	'halter top': ["top", 20, 45],
-	'tube top': ["top", 20, 45],
-	'sports bra': ["top", 30, 45],
+    //top
+    'tank top': ["top",  20, 45],
+    't shirt': ["top", 20, 45],
+    'tee': ["top", 20, 45],
+    'shirt':["top", 20, 45],
+    'long sleeves shirt': ["top", 10, 20],
+    'long sleeve shirt': ["top", 10, 20],
+    'long sleeved shirt': ["top", 10, 20],
+    'long sleeve dress shirt': ["top", 10, 20],
+    'long sleeves dress shirt': ["top", 10, 20],
+    'long sleeved dress shirt': ["top", 10, 20],
+    'blouse': ["top", 10, 20],
+    'sweater': ["top", 0, 10],
+    'sweatshirt': ["top", 10, 20],
+    'dress shirt': ["top", "formal", -30, 20],
+    'dress T shirt': ["top", 20, 45],
+    'polo': ["top", 20, 45],
+    'crop top': ["top", 20, 45],
+    'halter top': ["top", 20, 45],
+    'tube top': ["top", 20, 45],
+    'sports bra': ["top", 30, 45],
 
-	//layer1
-	'sweater': ["top", "semi-formal", 0, 10],
-	'poncho': ["top", "casual", 30, 45],
-	'sweater vest': ["top", "semi formal", 10, 20],
-	'vest': ["top", "formal", 10, 20],
-	'cardigan': ["top", "semi-formal", 0, 20],
+    //layer1
+    'sweater': ["top", "semi-formal", 0, 10],
+    'poncho': ["top", "casual", 30, 45],
+    'sweater vest': ["top", "semi formal", 10, 20],
+    'vest': ["top", "formal", 10, 20],
+    'cardigan': ["top", "semi-formal", 0, 20],
 
-	//bottom
-	'jeans': ["bottom", -30, 20],
-	'mini skirt': ["bottom", 20, 30],
-	'skirt': ["bottom", 10, 20],
-	'pants': ["bottom", -30, 20],
-	'trousers': ["bottom", -30, 20],
-	'slacks': ["bottom", 0, 20],
-	'shorts': ["bottom", 20, 45],
-	'dress pants': ["bottom", 0, 20],
-	'capri pants': ["bottom", 0, 20],
-	'overalls': ["bottom", -30, 0],
-	'yoga pants': ["bottom", 10, 20],
-	'leather pants': ["bottom", 0, 10],
-	'snow pants': ["bottom", -30, -20],
-	'track pants': ["bottom", 10, 30],
-	'dress pants': ["bottom", -10, 30],
-	'sweatpants': ["bottom", 10, 30],
-'sweats': ["bottom", 10, 30],
+    //bottom
+    'jeans': ["bottom", -30, 20],
+    'mini skirt': ["bottom", 20, 30],
+    'skirt': ["bottom", 10, 20],
+    'pants': ["bottom", -30, 20],
+    'trousers': ["bottom", -30, 20],
+    'slacks': ["bottom", 0, 20],
+    'shorts': ["bottom", 20, 45],
+    'dress pants': ["bottom", 0, 20],
+    'capri pants': ["bottom", 0, 20],
+    'overalls': ["bottom", -30, 0],
+    'yoga pants': ["bottom", 10, 20],
+    'leather pants': ["bottom", 0, 10],
+    'snow pants': ["bottom", -30, -20],
+    'track pants': ["bottom", 10, 30],
+    'dress pants': ["bottom", -10, 30],
+    'sweatpants': ["bottom", 10, 30],
+    'sweats': ["bottom", 10, 30],
 
-	//all
-	'dress': ["all", -20, 20],
-  'sundress': ["all", -20, 20],
-  'slip dress': ["all", -20, 20],
-  'strapless dress': ["all", -20, 20],
-	'short dress': ["all", 0, 20],
-	'gown': ["all", -30, 20],
+    //all
+    'dress': ["all", -20, 20],
+    'sundress': ["all", -20, 20],
+    'slip dress': ["all", -20, 20],
+    'strapless dress': ["all", -20, 20],
+    'short dress': ["all", 0, 20],
+    'gown': ["all", -30, 20],
 }
 
 function parseClothes(text){
-  text = text.toLowerCase().replace('.', '');
-  var color, description, article;
-  var parsedText = text.split(" ");
-  var colorIndex, descriptionIndex, articleIndex;
-  var inIndex = parsedText.indexOf("in");
+    text = text.toLowerCase().replace('.', '');
+    var color, description, article;
+    var parsedText = text.split(" ");
+    var colorIndex, descriptionIndex, articleIndex;
+    var inIndex = parsedText.indexOf("in");
 
-  if(inIndex > -1){
-    colorIndex = parsedText.indexOf("in") + 1;
-    articleIndex = parsedText.indexOf("in");
-
-  }
-  else {
-    colorIndex = 0;
-    articleIndex = parsedText.length;
-    descriptionIndex = articleIndex;
-  }
+    if(inIndex > -1){
+        colorIndex = parsedText.indexOf("in") + 1;
+        articleIndex = parsedText.indexOf("in");
+    } else {
+        colorIndex = 0;
+        articleIndex = parsedText.length;
+        descriptionIndex = articleIndex;
+    }
     if (colorQualifiers.indexOf(parsedText[colorIndex]) > -1)
-      color = parsedText[colorIndex] + " "  + parsedText[colorIndex+1];
+        color = parsedText[colorIndex] + " "  + parsedText[colorIndex+1];
     else
-      color = parsedText[colorIndex];
+        color = parsedText[colorIndex];
 
     if(clothes.hasOwnProperty(parsedText[articleIndex - 4] + " " + parsedText[articleIndex - 3] + " " + parsedText[articleIndex - 2] + " " + parsedText[articleIndex - 1]))
-      article = parsedText[articleIndex - 4] + " " + parsedText[articleIndex - 3] + " " + parsedText[articleIndex - 2] + " " + parsedText[articleIndex- 1];
+        article = parsedText[articleIndex - 4] + " " + parsedText[articleIndex - 3] + " " + parsedText[articleIndex - 2] + " " + parsedText[articleIndex- 1];
     else if(clothes.hasOwnProperty(parsedText[articleIndex - 3] + " " + parsedText[articleIndex - 2] + " " + parsedText[articleIndex - 1]))
-       article = parsedText[articleIndex - 3] + " " + parsedText[articleIndex - 2] + " " + parsedText[articleIndex - 1];
+        article = parsedText[articleIndex - 3] + " " + parsedText[articleIndex - 2] + " " + parsedText[articleIndex - 1];
     else if(clothes.hasOwnProperty(parsedText[articleIndex - 2] + " " + parsedText[articleIndex - 1]))
-      article = parsedText[articleIndex - 2] + " " + parsedText[articleIndex - 1];
+        article = parsedText[articleIndex - 2] + " " + parsedText[articleIndex - 1];
     else if (clothes.hasOwnProperty(parsedText[articleIndex - 1]))
-      article = parsedText[articleIndex - 1];
+        article = parsedText[articleIndex - 1];
     else
-      article = "Error";
+        article = "Error";
 
     if (inIndex === -1)
-      description = parsedText.slice(color.split(" ").length, descriptionIndex - article.split(" ").length);
+        description = parsedText.slice(color.split(" ").length, descriptionIndex - article.split(" ").length);
     else{
-       description = parsedText.slice(0, inIndex - article.split(" ").length);
+        description = parsedText.slice(0, inIndex - article.split(" ").length);
     }
 
-  return {
-    'color': color,
-    'description': description,
-    'article': article
-  }
+    return {
+        'color': color,
+        'description': description,
+        'article': article
+    }
 }
